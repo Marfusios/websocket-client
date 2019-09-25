c3e93cf3581a95e0eb55579207e786d86e598aa0
Søren Bjergmark <sahb1239@gmail.com>
2019-09-25 22:17:48 +0200

    Added testcase

---
+++ test/Websocket.Client.Tests/Testserver/TestserverApplicationFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.TestHost;
+
+namespace Websocket.Client.Tests.Testserver
+{
+    public class TestserverApplicationFactory<T> : WebApplicationFactory<T>
+        where T : class
+    {
+        protected override TestServer CreateServer(IWebHostBuilder builder) =>
+            base.CreateServer(
+                builder.UseSolutionRelativeContentRoot(""));
+
+        protected override IWebHostBuilder CreateWebHostBuilder() =>
+            WebHost.CreateDefaultBuilder()
+                .UseStartup<T>();
+    }
+}
---
+++ test/Websocket.Client.Tests/WebsocketClientTests.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.WebSockets;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Websocket.Client.Tests.Testserver;
+using Xunit;
+
+namespace Websocket.Client.Tests
+{
+    public class WebsocketClientTests
+    {
+        private readonly TestserverApplicationFactory<EchoTestServerStartup> _factory;
+
+        public WebsocketClientTests()
+        {
+            _factory = new TestserverApplicationFactory<EchoTestServerStartup>();
+        }
+
+        private IWebsocketClient CreateClient()
+        {
+            var httpClient = _factory.CreateClient(); // This is needed since _factory.Server would otherwise be null
+            var wsUri = new UriBuilder(_factory.Server.BaseAddress)
+            {
+                Scheme = "ws",
+                Path = "ws"
+            }.Uri;
+            return new WebsocketClient(wsUri,
+                async (uri, token) => 
+                { 
+                    var client = _factory.Server.CreateWebSocketClient();
+                    return await client.ConnectAsync(uri, token).ConfigureAwait(false);
+                });
+        }
+
+        [Fact]
+        public async Task Recieves_Echo()
+        {
+            using (IWebsocketClient client = CreateClient())
+            {
+                string received = null;
+                var receivedCount = 0;
+                var receivedEvent = new ManualResetEvent(false);
+                
+                client
+                    .MessageReceived
+                    .Where(x => x.Text.ToLower().Contains("ping"))
+                    .Subscribe(msg =>
+                    {
+                        receivedCount++;
+                        received = msg.Text;
+
+                        receivedEvent.Set();
+                    });
+
+                await client.Start();
+
+                await client.Send("ping");
+
+                receivedEvent.WaitOne(TimeSpan.FromSeconds(30));
+
+                Assert.NotNull(received);
+            }
+        }
+
+        // This is from https://github.com/aspnet/AspNetCore.Docs/blob/master/aspnetcore/fundamentals/websockets/samples/2.x/WebSocketsSample/Startup.cs
+        public class EchoTestServerStartup
+        {
+            public void ConfigureServices(IServiceCollection services)
+            {
+
+            }
+
+            public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+            {
+                app.UseWebSockets();
+                app.Use(async (context, next) =>
+                {
+                    if (context.Request.Path == "/ws")
+                    {
+                        if (context.WebSockets.IsWebSocketRequest)
+                        {
+                            WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                            await Echo(context, webSocket);
+                        }
+                        else
+                        {
+                            context.Response.StatusCode = 400;
+                        }
+                    }
+                    else
+                    {
+                        await next();
+                    }
+                });
+            }
+
+            private async Task Echo(HttpContext context, WebSocket webSocket)
+            {
+                var buffer = new byte[1024 * 4];
+                WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                while (!result.CloseStatus.HasValue)
+                {
+                    await webSocket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+                    result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+                }
+                await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            }
+        }
+    }
+
+
+}
